f70ae6d556147fdf63167c48045473395de870cf
Nicholas Padmanabhan <13815069+nicholaspad@users.noreply.github.com>
2022-02-09 16:05:40 -0500

    CAS authentication & user creation (#1)

---
+++ hooks/useCAS.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
+import useSWR from "swr";
+
+// hook to manage Princeton CAS authentication
+// example usage: const { isLoading, netID } = useCAS();
+export default function useCAS() {
+  const router = useRouter();
+  const fetcher = (url: string) => fetch(url).then((r) => r.json());
+  const [ticket, setTicket] = useState("");
+  // call /api/auth endpoint to verify ticket, exchange for a netID,
+  // save that netID, and create a user if necessary
+  const { data, error } = useSWR(
+    ticket ? `/api/auth?ticket=${ticket}` : "/api/auth",
+    fetcher
+  );
+  const isLoggedIn = !error && data && "netid" in data;
+
+  // extract ticket from URL (passed by CAS server)
+  useEffect(() => {
+    const { ticket } = router.query;
+    setTicket(ticket as string);
+    if (data && !isLoggedIn && router.pathname != "/") router.push("/");
+  });
+
+  return {
+    isLoggedIn: isLoggedIn,
+    isLoading: !data,
+    netID: isLoggedIn ? data["netid"] : "",
+  };
+}
