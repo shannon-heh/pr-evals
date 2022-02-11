830c2a5d91add1568dc2307c01a5a17b2686b0b6
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-02-10 21:05:19 -0500

    Refactor loading spinner into own component

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -4,6 +4,7 @@ import CircularProgress from "@mui/material/CircularProgress";
 import CustomHead from "../components/CustomHead";
 import { useRouter } from "next/router";
 import useSWR from "swr";
+import Loading from "../components/Loading";
 
 export default function Logout() {
   const router = useRouter();
@@ -13,24 +14,5 @@ export default function Logout() {
 
   if (loggedOut) router.push("/");
 
-  return (
-    <>
-      <CustomHead pageTitle="Loading" />
-      <Container maxWidth={false}>
-        <Grid
-          container
-          spacing={0}
-          direction="column"
-          alignItems="center"
-          justifyContent="center"
-          style={{ minHeight: "100vh" }}
-        >
-          <Grid item xs={3} sx={{ textAlign: "center" }}>
-            <CircularProgress />
-            <h2>Logging out...</h2>
-          </Grid>
-        </Grid>
-      </Container>
-    </>
-  );
+  return <Loading text={"Logging out..."} />;
 }
