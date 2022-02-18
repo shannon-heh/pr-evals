64136b8f059c4876ff959cba4082d99ffa66fe2a
shannon-heh <shannon.heh@princeton.edu>
2022-02-17 23:39:12 -0500

    add custom head to profile page

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -8,6 +8,7 @@ import Typography from "@mui/material/Typography";
 import { useFormik } from "formik";
 import useSWR from "swr";
 import useCAS from "../hooks/useCAS";
+import CustomHead from "../components/CustomHead";
 
 // DB fields for student user
 type StudentData = {
@@ -62,65 +63,68 @@ export default function Profile() {
 
   const user: StudentData = userData.data;
   return (
-    <Grid
-      container
-      direction="column"
-      alignItems="center"
-      justifyContent="center"
-      style={{ minHeight: "100vh" }}
-    >
-      <Grid item sx={{ fontWeight: "bolder" }}>
-        <Typography variant="h4" id="user-name">
-          {user.name}
-        </Typography>
-      </Grid>
-      <Grid item sx={{ fontStyle: "italic", marginBottom: "1rem" }}>
-        <Typography variant="h5" id="user-netid">
-          {user.netid}
-        </Typography>
-      </Grid>
-      <Grid item>
-        <Typography id="user-year">Year: {user.class_year}</Typography>
-      </Grid>
-      <Grid item>
-        <Typography id="user-type">
-          Type: {capitalize(user.person_type)}
-        </Typography>
-      </Grid>
+    <>
+      <CustomHead pageTitle="Profile" />
+      <Grid
+        container
+        direction="column"
+        alignItems="center"
+        justifyContent="center"
+        style={{ minHeight: "100vh" }}
+      >
+        <Grid item sx={{ fontWeight: "bolder" }}>
+          <Typography variant="h4" id="user-name">
+            {user.name}
+          </Typography>
+        </Grid>
+        <Grid item sx={{ fontStyle: "italic", marginBottom: "1rem" }}>
+          <Typography variant="h5" id="user-netid">
+            {user.netid}
+          </Typography>
+        </Grid>
+        <Grid item>
+          <Typography id="user-year">Year: {user.class_year}</Typography>
+        </Grid>
+        <Grid item>
+          <Typography id="user-type">
+            Type: {capitalize(user.person_type)}
+          </Typography>
+        </Grid>
 
-      <FormControl sx={{ flexDirection: "row", mt: "1rem", width: "250px" }}>
-        <InputLabel id="major-input-label">Concentration</InputLabel>
-        <Select
-          labelId="major-input-label"
-          id="user-major-select"
-          name="major"
-          label="Concentration"
-          value={formik.values.major}
-          onChange={formik.handleChange}
-          sx={{ width: "75%", mr: "5px" }}
-        >
-          {deptItems}
-          <MenuItem key="Other" value="Other">
-            Other
-          </MenuItem>
-          <MenuItem key="None" value="None">
-            None
-          </MenuItem>
-        </Select>
-        <Button
-          color="primary"
-          variant="contained"
-          fullWidth
-          type="submit"
-          onClick={(e) => {
-            e.preventDefault();
-            formik.handleSubmit();
-          }}
-          sx={{ width: "25%" }}
-        >
-          Save
-        </Button>
-      </FormControl>
-    </Grid>
+        <FormControl sx={{ flexDirection: "row", mt: "1rem", width: "250px" }}>
+          <InputLabel id="major-input-label">Concentration</InputLabel>
+          <Select
+            labelId="major-input-label"
+            id="user-major-select"
+            name="major"
+            label="Concentration"
+            value={formik.values.major}
+            onChange={formik.handleChange}
+            sx={{ width: "75%", mr: "5px" }}
+          >
+            {deptItems}
+            <MenuItem key="Other" value="Other">
+              Other
+            </MenuItem>
+            <MenuItem key="None" value="None">
+              None
+            </MenuItem>
+          </Select>
+          <Button
+            color="primary"
+            variant="contained"
+            fullWidth
+            type="submit"
+            onClick={(e) => {
+              e.preventDefault();
+              formik.handleSubmit();
+            }}
+            sx={{ width: "25%" }}
+          >
+            Save
+          </Button>
+        </FormControl>
+      </Grid>
+    </>
   );
 }
