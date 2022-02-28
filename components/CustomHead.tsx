68fc01590ba768df5e30dabafb3f4da0c13b0605
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-02-28 01:06:33 -0500

    Fix error with stylesheet link tag placement

--- components/CustomHead.tsx
+++ components/CustomHead.tsx
@@ -6,10 +6,6 @@ export default function CustomHead(props: { pageTitle?: string }) {
       <title>
         Course Evals IW {props.pageTitle ? "| " + props.pageTitle : ""}
       </title>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
       <meta name="viewport" content="initial-scale=1, width=device-width" />
     </Head>
   );
