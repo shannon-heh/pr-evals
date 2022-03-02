2a8e15e03df3647ce7c62b3b303122417798b5d2
shannon-heh <63625700+shannon-heh@users.noreply.github.com>
2022-03-01 22:57:41 -0500

    Setup Navbar (#11)
    * add basic nav bar
    
    * add reload to ensure login status is updated

--- components/CustomHead.tsx
+++ components/CustomHead.tsx
@@ -1,16 +1,20 @@
 import Head from "next/head";
+import NavBar from "./NavBar";
 
 export default function CustomHead(props: { pageTitle?: string }) {
   return (
-    <Head>
-      <title>
-        Course Evals IW {props.pageTitle ? "| " + props.pageTitle : ""}
-      </title>
-      <link
-        rel="stylesheet"
-        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
-      />
-      <meta name="viewport" content="initial-scale=1, width=device-width" />
-    </Head>
+    <>
+      <Head>
+        <title>
+          Course Evals IW {props.pageTitle ? "| " + props.pageTitle : ""}
+        </title>
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+        />
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <NavBar />
+    </>
   );
 }
