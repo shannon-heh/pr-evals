a9d6bb3d48ceaaa6432589aa78865bb30dbd9205
Nicholas Padmanabhan <nicholaspad@gmail.com>
2022-03-26 00:17:50 -0400

    Modularize Filters component

--- components/course/Charts.tsx
+++ components/course/Charts.tsx
@@ -1,27 +1,16 @@
-import {
-  Box,
-  Button,
-  FormControl,
-  Grid,
-  InputLabel,
-  MenuItem,
-  Select,
-  SelectChangeEvent,
-  Skeleton,
-  Typography,
-} from "@mui/material";
+import { Box, Grid, Skeleton, Typography } from "@mui/material";
 import { blue, red } from "@mui/material/colors";
 import { useState } from "react";
 import useSWR from "swr";
 import useWindowDimensions from "../../hooks/windowDimensions";
 import { COLORS, fetcher } from "../../src/Helpers";
 import { ChartData, ResponseData } from "../../src/Types";
-import DeleteIcon from "@mui/icons-material/Delete";
 import MultiChoiceChart from "./charts/MultiChoiceChart";
 import ScaleChart from "./charts/ScaleChart";
 import SingleChoiceChart from "./charts/SingleChoiceChart";
 import TextChart from "./charts/TextChart";
 import HoverCard from "./HoverCard";
+import Filters from "./Filters";
 
 export default function Charts(props: {
   courseID?: string;
@@ -215,86 +204,6 @@ export default function Charts(props: {
         </Grid>
       )}
     </>
-  );
-}
-
-function Filters(props: {
-  setConcentrationFilter: Function;
-  concentrationFilter: string;
-  setYearFilter: Function;
-  yearFilter: string;
-}) {
-  const { data: dataMajors, error: errorMajors } = useSWR(
-    "/api/get-majors",
-    fetcher
-  );
-  const { data: dataYears, error: errorYears } = useSWR(
-    "/api/get-class-years",
-    fetcher
-  );
-
-  const handleConcentrationChange = (event: SelectChangeEvent) => {
-    props.setConcentrationFilter(event.target.value as string);
-  };
-  const handleYearChange = (event: SelectChangeEvent) => {
-    props.setYearFilter(event.target.value as string);
-  };
-  const handleFilterReset = () => {
-    props.setYearFilter("");
-    props.setConcentrationFilter("");
-  };
-
-  return (
-    <Box
-      sx={{
-        mt: 2,
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <FormControl sx={{ width: 200 }}>
-        <InputLabel>Select Concentration</InputLabel>
-        <Select
-          value={props.concentrationFilter}
-          label="Select Concentration"
-          onChange={handleConcentrationChange}
-        >
-          {dataMajors?.majors.map((major: string) => (
-            <MenuItem key={major} value={major}>
-              {major}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl sx={{ ml: 2, width: 200 }}>
-        <InputLabel>Select Year</InputLabel>
-        <Select
-          value={props.yearFilter}
-          label="Select Year"
-          onChange={handleYearChange}
-        >
-          {dataYears?.map((year: string) => (
-            <MenuItem key={year} value={year}>
-              {year}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
-      <FormControl sx={{ ml: 2, width: 100 }}>
-        <Button
-          startIcon={<DeleteIcon />}
-          variant="outlined"
-          size="large"
-          color="error"
-          onClick={() => {
-            handleFilterReset();
-          }}
-        >
-          Reset
-        </Button>
-      </FormControl>
-    </Box>
   );
 }
 
