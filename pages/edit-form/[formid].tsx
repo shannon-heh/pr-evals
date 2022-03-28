b820f69316bca3f52b1949352e6af022574de488
shannon-heh <shannon.heh@princeton.edu>
2022-03-28 01:37:44 -0400

    set up accordion interface of sample q's

--- pages/edit-form/[formid].tsx
+++ pages/edit-form/[formid].tsx
@@ -23,6 +23,11 @@ import useCAS from "../../hooks/useCAS";
 import DeleteForeverIcon from "@mui/icons-material/DeleteForever";
 import IconButton from "@mui/material/IconButton";
 import Tooltip from "@mui/material/Tooltip";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useFormik } from "formik";
 
 // Page for instructor to edit a form
 export default function EditForm() {
@@ -270,6 +275,7 @@ export default function EditForm() {
               Click 'Cancel' to continue editing.
             </ConfirmationDialog>
           </Grid>
+          <SampleQuestions />
           <Grid item container flexDirection="column" sx={{ pb: 2 }}>
             {questions.map((q: QuestionMetadata, i: number) => {
               let input = null;
@@ -345,5 +351,129 @@ export default function EditForm() {
         </Grid>
       </Grid>
     </>
+  );
+}
+
+function SampleQuestions() {
+  // open is true when sample questions are shown
+  const [openQuestions, setOpen] = useState(false);
+
+  // get sample questions
+  const { data: questions, error: questionsError } = useSWR(
+    `/api/get-sample-questions`,
+    fetcher
+  );
+
+  // show/hide accordion of question categories
+  const openAccordion = () => {
+    setOpen(true);
+  };
+  const closeAccordion = () => {
+    setOpen(false);
+  };
+
+  if (questionsError) return <div></div>;
+
+  // question categories
+  const categories: string[] = Object.keys(questions?.sample_questions ?? {});
+
+  return (
+    <Grid container item justifyContent={"center"}>
+      <Button
+        variant="contained"
+        onClick={openQuestions ? closeAccordion : openAccordion}
+        sx={{ px: 4, width: "95%", mb: 2 }}
+      >
+        {openQuestions ? "Close Sample Questions" : "View Sample Questions"}
+      </Button>
+      {openQuestions ? (
+        <Grid sx={{ mt: 1, my: 2 }}>
+          {categories.map((category: string, i: number) => {
+            return (
+              <Accordion key={i} sx={{ backgroundColor: blue[50] }}>
+                <AccordionSummary
+                  expandIcon={<ExpandMoreIcon />}
+                  aria-controls="panel1a-content"
+                  id="panel1a-header"
+                  sx={{ height: "40px" }}
+                >
+                  <Typography>{category}</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                  {questions.sample_questions[category].map(
+                    (q: QuestionMetadata, i: number) => {
+                      let input = null;
+                      if (q.type == Question.ShortText) {
+                        input = <ShortTextInput />;
+                      } else if (q.type == Question.LongText) {
+                        input = <LongTextInput />;
+                      } else if (q.type == Question.SingleSelect) {
+                        input = <SingleSelectInput options={q.options} />;
+                      } else if (q.type == Question.MultiSelect) {
+                        input = <MultiSelectInput options={q.options} />;
+                      } else if (q.type == Question.Slider) {
+                        input = (
+                          <SliderInput
+                            min={q.min}
+                            max={q.max}
+                            step={q.step}
+                            marks={q.marks}
+                          />
+                        );
+                      } else if (q.type == Question.Rating) {
+                        input = (
+                          <RatingInput max={q.max} precision={q.precision} />
+                        );
+                      }
+                      return (
+                        <Grid
+                          item
+                          container
+                          key={i}
+                          sx={{
+                            my: 1.5,
+                            p: 1.5,
+                            backgroundColor: grey[50],
+                            borderRadius: "8px",
+                            flexDirection: "column",
+                            pb: q.type == Question.Slider ? 4.5 : 1.5,
+                          }}
+                        >
+                          <Grid
+                            container
+                            item
+                            flexDirection="row"
+                            justifyContent="space-between"
+                            alignItems="center"
+                            sx={{ flexWrap: "nowrap" }}
+                          >
+                            <Typography
+                              variant="body1"
+                              sx={{ overflow: "auto" }}
+                            >
+                              {q.question}
+                            </Typography>
+                          </Grid>
+                          {q.description != "" ? (
+                            <Typography
+                              variant="caption"
+                              mb={1}
+                              sx={{ width: "100%", overflow: "auto" }}
+                            >
+                              {q.description}
+                            </Typography>
+                          ) : null}
+                          {input}
+                        </Grid>
+                      );
+                    }
+                  )}
+                </AccordionDetails>
+              </Accordion>
+            );
+          })}
+        </Grid>
+      ) : null}
+    </Grid>
   );
 }
